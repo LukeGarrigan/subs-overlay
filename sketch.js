d3c49dbebe3350b37689037c457d20bbb91c60dc
Luke Garrigan <lukegarrigan8@gmail.com>
2018-12-28 23:34:37 +0000

    Added all types of rockets

--- sketch.js
+++ sketch.js
@@ -16,24 +16,25 @@ function setup() {
   // let subscriberList = loadJSON("https://api.twitch.tv/kraken/channels/240986836/subscriptions");
   let bronzeSpaceship = loadImage("images/Bronze_ship.png");
   let silverSpaceship = loadImage("images/Silver_ship.png");
-
+  let goldSpaceship = loadImage("images/Gold_ship.png");
+  let diamondSpaceship = loadImage("images/Diamond_ship.png");
   for (let sub of subs) {
     let subscriberInfo = sub.split(',');
     let name = subscriberInfo[0];
     let dateSubbed = subscriberInfo[1];
-
-
     let date = Date.parse(dateSubbed);
 
     let spaceship;
-    if (Date.now() - date > 7776000000 ) {
+    if (name === 'codeheir') {
+      spaceship = new Spaceship(name,diamondSpaceship);
+    } else if (Date.now() - date > (7776000000*2) ) {
+       spaceship = new Spaceship(name,goldSpaceship);
+    } else if (Date.now() - date > 7776000000){
        spaceship = new Spaceship(name,silverSpaceship);
     } else {
        spaceship = new Spaceship(name,bronzeSpaceship);
     }
-
     spaceships.push(spaceship);
-
   }
 }
 
