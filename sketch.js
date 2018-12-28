287c30385a5a29aac99a450ed96b800587aa51d3
Luke Garrigan <lukegarrigan8@gmail.com>
2018-12-28 23:19:27 +0000

    Added a seperate function for the displaying the members count

--- sketch.js
+++ sketch.js
@@ -62,14 +62,18 @@ function draw() {
     snow.draw();
   }
 
+  displayMembersCount();
 
+}
 
+function displayMembersCount() {
   push();
   fill(255);
   textAlign(CENTER);
   textSize(15);
   text(subs.length +" current members", width/2 , height-20);
   pop();
+
 }
 
 
