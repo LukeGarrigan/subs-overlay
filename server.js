83e7ebee831397e274a7fc66f24407719bb9d1e9
Luke Garrigan <lukegarrigan8@gmail.com>
2019-01-01 17:46:19 +0000

    Made initial db connection

--- server.js
+++ server.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const sql = require('mssql');
 
 var app = express();
 app.use(express.static('public'));
 
 var server = app.listen(9999);
+
+
+
+console.log("starting");
+
+var config = {
+  server: 'localhost',
+  database: 'todone',
+  user: 'programmer',
+  password: 'test',
+};
+connect();
+
+
+
+async function connect() {
+  try {
+    console.log("Connecting to db");
+    await sql.connect(config);
+    let value = 1;
+    const result = await sql.query`select * from task where id = ${value}`
+    console.dir(result)
+  } catch (err) {
+    // ... error checks
+    console.log(err);
+  }
+}
