7c71e923f081d5eb0275ef4637110bca82d39676
Luke Garrigan <lukegarrigan8@gmail.com>
2019-01-01 16:52:03 +0000

    Got rid of unnecessary stuff

--- server.js
+++ server.js
@@ -1,26 +1,6 @@
 const express = require('express');
-const cors = require('cors');
+
 var app = express();
-
-app.use(cors());
-
-
-
 app.use(express.static('public'));
 
 var server = app.listen(9999);
-
-
-
-//
-// var socket = require('socket.io');
-
-// var io = socket(server);
-
-// io.sockets.on('connection', connectionMade)
-//
-// function connectionMade(socket) {
-//
-//   console.log("connection " + socket);
-//
-// }
